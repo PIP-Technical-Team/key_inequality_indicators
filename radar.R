5aee7e8a60189d20883760872ac15a9fa5e4c4f2
zander-prinsloo <prinsloo.zander@gmail.com>
2023-09-20 14:55:51 -0400

    initial radar/web plot

--- radar.R
+++ radar.R
@@ -1,66 +1,105 @@
-# Library
+dt_use <- copy(dt_pip)
 pacman::p_load(fmsb)
+
+
+
+dt_use2 <- left_join(
+create_data_imputed_by_countries(
+  dt = dt_use, 
+  countries_selected = pip_countries, 
+  year_selected = 1998, 
+  indicator = c("Mean")
+) |> select(country_name, year, mean), 
+create_data_imputed_by_countries(
+  dt = dt_use, 
+  countries_selected = pip_countries, 
+  year_selected = 1998, 
+  indicator = c("Median")
+) |> select(country_name, year, median)) |> 
+  left_join(
+    create_data_imputed_by_countries(
+      dt = dt_use, 
+      countries_selected = pip_countries, 
+      year_selected = 1998, 
+      indicator = c("Gini")
+    ) |> select(country_name, year, gini)) |> 
+  left_join(
+    create_data_imputed_by_countries(
+      dt = dt_use, 
+      countries_selected = pip_countries, 
+      year_selected = 1998, 
+      indicator = c("Mld")
+    ) |> select(country_name, year, mld)) |> 
+  left_join(
+    create_data_imputed_by_countries(
+      dt = dt_use, 
+      countries_selected = pip_countries, 
+      year_selected = 1998, 
+      indicator = c("Polarization")
+    ) |> select(country_name, year, polarization)) |> 
+  left_join(
+    create_data_imputed_by_countries(
+      dt = dt_use, 
+      countries_selected = pip_countries, 
+      year_selected = 1998, 
+      indicator = c("Top10_bottom40")
+    ) |> select(country_name, year, top10_bottom40)) |> 
+  left_join(
+    create_data_imputed_by_countries(
+      dt = dt_use, 
+      countries_selected = pip_countries, 
+      year_selected = 1998, 
+      indicator = c("Top20_bottom20")
+    ) |> select(country_name, year, top20_bottom20)) |> 
+  left_join(
+    create_data_imputed_by_countries(
+      dt = dt_use, 
+      countries_selected = pip_countries, 
+      year_selected = 1998, 
+      indicator = c("Headcount")
+    ) |> select(country_name, year, headcount)) |> 
+  left_join(
+    create_data_imputed_by_countries(
+      dt = dt_use, 
+      countries_selected = pip_countries, 
+      year_selected = 1998, 
+      indicator = c("Poverty_gap")
+    ) |> select(country_name, year, poverty_gap)) |> 
+  left_join(
+    create_data_imputed_by_countries(
+      dt = dt_use, 
+      countries_selected = pip_countries, 
+      year_selected = 1998, 
+      indicator = c("Poverty_severity")
+    ) |> select(country_name, year, poverty_severity))
+
+
+dt_use2 |> 
+  head() |> 
+  select(country_name, year, mean) |> 
+  mutate(
+    mean = mean/max(mean)
+  )
+
+
+
+
+
+
+
+# Custom the radarChart !
+radarchart( dt_use
+)
 
 # Create data: note in High school for Jonathan:
 data <- as.data.frame(matrix( sample( 2:20 , 10 , replace=T) , ncol=10))
 colnames(data) <- c("math" , "english" , "biology" , "music" , "R-coding", "data-viz" , "french" , "physic", "statistic", "sport" )
 
 # To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
-data <- rbidt_use <- copy(dt_pip)
-dt_use2 <- copy(dt_pip)
+data <- rbind(rep(20,10) , rep(0,10) , data)
+data
+radarchart(data |> select(-c(`data-viz`, music, math)))
 
-dt_use <- create_data_ranked_by_countries(
-  dt = dt_use, 
-  countries_selected = pip_countries, 
-  year_selected = 2000, 
-  indicator = "Gini"
-)
 
-dt_use3 <- joyn::merge(
-  dt_use, 
-  dt_use2 |> rename("Gini2" = "gini") |> filter(year == 2005), 
-  match_type = "1:1", 
-  keep = "left", 
-  yvar = "Gini2", 
-  by = c("country_name", "welfare_type", "reporting_level")
-) |> 
-  mutate(
-    GiniChange = gini - Gini2
-  ) |>
-  filter(!is.na(GiniChange)) |> 
-  mutate(
-    Change = ifelse(GiniChange<0, "Negative", "Positive")
-  ) 
-dt_use3[, country_name := factor(country_name, levels = country_name[order(Rank)])]
-
-dt_use3 |>
-  ggplot() +
-  geom_segment( aes(x=0, xend=GiniChange, y=country_name, yend=country_name, color = Change)) + 
-  geom_point(aes(x = GiniChange, y = country_name, color = Change))+
-  scale_color_manual(values = c("Negative" = "red", "Positive" = "darkgreen")) + 
-  my_theme()+
-  labs(
-    title = "Change in Gini from 2000 to 2005",
-    subtitle = "Countries ordered from highest to lowest 2000 Gini", 
-    x = "Change in Gini", 
-    y = "Country"
-  )
-  
-
-nd(rep(20,10) , rep(0,10) , data)
-
-# Check your data, it has to look like this!
-# head(data)
-
-# Custom the radarChart !
-radarchart( data  , axistype=1 , 
-            
-            #custom polygon
-            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
-            
-            #custom the grid
-            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
-            
-            #custom labels
-            vlcex=0.8 
-)
+dt_use |> 
+  filter(country_name = "South Africa")
